04329c0a54e65a2d891009fbf9947d21b01ee20b
JhoneM <jhone0901@gmail.com>
2020-12-15 17:09:18 -0300

    [UPD][purchase_order_origin_form_request] Incidence nbr./s: ODOO-540 - Remove wizard folder. - Update purchase_request create function. - Add views to make invisible origin field.

--- purchase_order_origin_from_request/__manifest__.py
+++ purchase_order_origin_from_request/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "http://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "11.0.1.0.1",
+    "version": "11.0.1.0.2",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
@@ -17,4 +17,7 @@
         "bin": [],
     },
     "depends": ["purchase_request"],
+    "data": [
+        "views/purchase_request.xml",
+    ],
 }
