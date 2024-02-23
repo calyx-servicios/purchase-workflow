690f8e58a6cb0abc4cf297fe7b3bf437dacec1e8
Leandro090685 <Leandro090685@github.com>
2024-02-23 17:07:34 -0300

    [FCO-322-2][FIX]

--- multi_warehouse_per_order/__manifest__.py
+++ multi_warehouse_per_order/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Purchase",
-    "version": "13.0.1.0.0",
+    "version": "13.0.2.0.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
